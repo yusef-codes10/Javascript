c5fc5a5a39d3f2e36c1da5c97fdf23119d37d10d
Youcef <youcefcopy@gmail.com>
2025-11-06 16:06:43 +0100

    variables has been added

--- Dragable/main.js
+++ Dragable/main.js
@@ -1,1 +1,7 @@
 // here is the main code
+
+// !1- setting some starting cordanants
+let newX = 0, newY = 0, startX = 0, startY = 0;
+
+// !2- declaring the card element in JS
+const card = document.getElementById('card');
