77f66c2d5867a7a1b7c4107e7ead85207f1ed169
Youcef <youcefcopy@gmail.com>
2025-11-06 16:15:56 +0100

    JS logic has been added

--- Dragable/main.js
+++ Dragable/main.js
@@ -1,7 +1,39 @@
 // here is the main code
 
 // !1- setting some starting cordanants
-let newX = 0, newY = 0, startX = 0, startY = 0;
+let newX = 0,
+  newY = 0,
+  startX = 0,
+  startY = 0;
 
 // !2- declaring the card element in JS
-const card = document.getElementById('card');
+const card = document.getElementById("card");
+
+card.addEventListener("mousedown", mouseDown);
+
+function mouseDown(e) {
+  startX = e.clientX;
+  startY = e.clientY;
+
+  card.addEventListener("mousemove", mouseMove);
+  card.addEventListener("mouseup", mouseUp);
+
+}
+
+function mouseMove(e) {
+    newX = startX - e.clientX
+    newY = startY - e.clientY
+
+    startX = e.clientX
+    startY = e.clientY
+
+    card.style.top = startY + 'px'
+    card.style.left = startX + 'px'
+
+    console.log({newX, newY})
+    console.log({startX, startY})
+
+
+
+
+}
