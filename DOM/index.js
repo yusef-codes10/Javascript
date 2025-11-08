cfae8647cbaf27b2f3c6c3af835ab953df799979
Youcef <youcefcopy@gmail.com>
2025-11-08 13:56:00 +0100

    DOM Manipulation 1st examole

--- DOM/index.js
+++ DOM/index.js
@@ -1,15 +1,29 @@
-// I- Add an element to the page
-// There are two ways to add elements to the page
-// 1- select the element you want to change, or to append elements to, speaking of append
-// append vs appendChild, with appendchild you can only append elements such as div, and other tags
-// with append you can append all of those with string any pretty much anything
-const body = document.body;
-body.append(`Hello`);
+// // I- Add an element to the page
+// // There are two ways to add elements to the page
+// // 1- select the element you want to change, or to append elements to, speaking of append
+// // append vs appendChild, with appendchild you can only append elements such as div, and other tags
+// // with append you can append all of those with string any pretty much anything
+// const body = document.body;
+// body.append(`Hello`);
 
-// ! II- Create an element and then you have to add it
-const div = document.createElement('div');
-body.append(div);
+// // ! II- Create an element and then you have to add it
+// const div = document.createElement('div');
+// body.append(div);
 
-// ! there are two way to add a text to a div
-div.innerText = "This is a DOM manipulation"
-div.textContent = "This is a DOM manipulation 2"
+// // ! there are two way to add a text to a div
+// div.innerText = "This is a DOM manipulation"
+// div.textContent = "This is a DOM manipulation 2"
+
+
+// ! DOM Manipulation
+
+
+// ! getElementById()
+
+// ! getElementByClassName()
+
+// ! getElementByTagName()
+
+// ! querySelector()
+
+// ! querySelectorAll()
