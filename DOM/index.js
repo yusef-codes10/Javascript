311820bf115160232935452023c3ffcc01ba2675
Youcef <youcefcopy@gmail.com>
2025-11-09 12:09:39 +0100

    selecting, adding and modifying elements updated example

--- DOM/index.js
+++ DOM/index.js
@@ -1,70 +1,21 @@
-// // // // I- Add an element to the page
-// // // // There are two ways to add elements to the page
-// // // // 1- select the element you want to change, or to append elements to, speaking of append
-// // // // append vs appendChild, with appendchild you can only append elements such as div, and other tags
-// // // // with append you can append all of those with string any pretty much anything
-// // // const body = document.body;
-// // // body.append(`Hello`);
-
-// // // // ! II- Create an element and then you have to add it
-// // // const div = document.createElement('div');
-// // // body.append(div);
-
-// // // // ! there are two way to add a text to a div
-// // // div.innerText = "This is a DOM manipulation"
-// // // div.textContent = "This is a DOM manipulation 2"
-
-
-// // // ! DOM Manipulation
-
-
-// // // ! getElementById()
-// // const title = document.getElementById('main-heading')
-// // console.log(title.textContent)
-
-// // // ! getElementByClassName()
-// // const list = document.getElementsByClassName('list-items');
-// // console.log(list)
-
-// // // ! getElementByTagName()
-// // const listItems = document.getElementsByTagName('li');
-// // console.log(listItems)
-
-// // // ! querySelector() | One item
-// // const container = document.querySelector('div')
-// // console.log(container)
-
-// // // ! querySelectorAll()
-
-// // ! 2- Styling an element
-// const title = document.querySelector('#main-heading');
-// title.style.color = 'red';
-// title.style.display = 'block';
-// title.style.fontSize = '3rem';
-
-// console.log(title);
-
-// // ! Inline CSS won't work with multiple items
-// const listItems = document.querySelectorAll('.list-items');
-// for (let i = 0; i < listItems.length; i++) {
-//     listItems[i].style.color = 'red';
-// }
-// console.log(listItems);
-
-
-//! Creating elements
+// ! Selecting an element (the ul)
 const ul = document.querySelector('ul');
-const xMen = document.querySelector('li');
-const transformers = document.querySelector('li');
+
+//! Creating elements (li)
+const xMen = document.createElement('li');
+const transformers = document.createElement('li');
 
 
 
-//! Adding elements
-ul.append(xMen);
+//! Adding elements (adding li items the list)
 ul.append(transformers);
+ul.append(xMen);
+
+
 
 
 // ! Modifying the text
-xMen.textContent = 'xmen';
 transformers.textContent = 'transformers';
+xMen.textContent = 'xmen';
+
 
