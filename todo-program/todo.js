8d1b22917a4442ee4537164d373a6c1ad4e604e9
Youcef <youcefcopy@gmail.com>
2025-11-09 12:39:42 +0100

    next steps

--- todo-program/todo.js
+++ todo-program/todo.js
@@ -1,17 +1,20 @@
-const add = document.getElementById('btn');
-const list = document.querySelector('ul');
-const todo = document.getElementById('text');
+const add = document.getElementById("btn");
+const list = document.querySelector("ul");
+const todo = document.getElementById("text");
 
 console.log(add);
 console.log(list);
 
-
 // add function
 add.onclick = function add() {
-    // we are going to create the list items here
-    const newItem = document.createElement('li');
-    // append
-    list.append(newItem);
-    // modify text content
-    newItem.textContent = todo.value;
-}
+  // we are going to create the list items here
+  const newItem = document.createElement("li");
+
+  // append
+  list.append(newItem);
+  // modify text content
+  newItem.textContent = todo.value;
+};
+
+// TODO add remove button, I should probably append an etire dive with its button
+// TODO update the style
