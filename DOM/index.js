0d43468b5ab9c58d11b7a204ef39ea635563473b
Youcef <youcefcopy@gmail.com>
2025-11-09 12:02:09 +0100

    slecting, styling, adding elements example

--- DOM/index.js
+++ DOM/index.js
@@ -1,52 +1,70 @@
-// // // I- Add an element to the page
-// // // There are two ways to add elements to the page
-// // // 1- select the element you want to change, or to append elements to, speaking of append
-// // // append vs appendChild, with appendchild you can only append elements such as div, and other tags
-// // // with append you can append all of those with string any pretty much anything
-// // const body = document.body;
-// // body.append(`Hello`);
+// // // // I- Add an element to the page
+// // // // There are two ways to add elements to the page
+// // // // 1- select the element you want to change, or to append elements to, speaking of append
+// // // // append vs appendChild, with appendchild you can only append elements such as div, and other tags
+// // // // with append you can append all of those with string any pretty much anything
+// // // const body = document.body;
+// // // body.append(`Hello`);
 
-// // // ! II- Create an element and then you have to add it
-// // const div = document.createElement('div');
-// // body.append(div);
+// // // // ! II- Create an element and then you have to add it
+// // // const div = document.createElement('div');
+// // // body.append(div);
 
-// // // ! there are two way to add a text to a div
-// // div.innerText = "This is a DOM manipulation"
-// // div.textContent = "This is a DOM manipulation 2"
-
-
-// // ! DOM Manipulation
+// // // // ! there are two way to add a text to a div
+// // // div.innerText = "This is a DOM manipulation"
+// // // div.textContent = "This is a DOM manipulation 2"
 
 
-// // ! getElementById()
-// const title = document.getElementById('main-heading')
-// console.log(title.textContent)
+// // // ! DOM Manipulation
 
-// // ! getElementByClassName()
-// const list = document.getElementsByClassName('list-items');
-// console.log(list)
 
-// // ! getElementByTagName()
-// const listItems = document.getElementsByTagName('li');
-// console.log(listItems)
+// // // ! getElementById()
+// // const title = document.getElementById('main-heading')
+// // console.log(title.textContent)
 
-// // ! querySelector() | One item
-// const container = document.querySelector('div')
-// console.log(container)
+// // // ! getElementByClassName()
+// // const list = document.getElementsByClassName('list-items');
+// // console.log(list)
 
-// // ! querySelectorAll()
+// // // ! getElementByTagName()
+// // const listItems = document.getElementsByTagName('li');
+// // console.log(listItems)
 
-// ! 2- Styling an element
-const title = document.querySelector('#main-heading');
-title.style.color = 'red';
-title.style.display = 'block';
-title.style.fontSize = '3rem';
+// // // ! querySelector() | One item
+// // const container = document.querySelector('div')
+// // console.log(container)
 
-console.log(title);
+// // // ! querySelectorAll()
 
-// ! Inline CSS won't work with multiple items
-const listItems = document.querySelectorAll('.list-items');
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].style.color = 'red';
-}
-console.log(listItems);
+// // ! 2- Styling an element
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// title.style.display = 'block';
+// title.style.fontSize = '3rem';
+
+// console.log(title);
+
+// // ! Inline CSS won't work with multiple items
+// const listItems = document.querySelectorAll('.list-items');
+// for (let i = 0; i < listItems.length; i++) {
+//     listItems[i].style.color = 'red';
+// }
+// console.log(listItems);
+
+
+//! Creating elements
+const ul = document.querySelector('ul');
+const xMen = document.querySelector('li');
+const transformers = document.querySelector('li');
+
+
+
+//! Adding elements
+ul.append(xMen);
+ul.append(transformers);
+
+
+// ! Modifying the text
+xMen.textContent = 'xmen';
+transformers.textContent = 'transformers';
+
