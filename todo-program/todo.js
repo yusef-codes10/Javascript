7223871eff77faf7c8a62192143f8c12855bad83
Youcef <youcefcopy@gmail.com>
2025-11-09 12:30:22 +0100

    button functionality has been updated | creating list items

--- todo-program/todo.js
+++ todo-program/todo.js
@@ -1,5 +1,17 @@
 const add = document.getElementById('btn');
 const list = document.querySelector('ul');
+const text = document.getElementById('text');
 
 console.log(add);
 console.log(list);
+
+
+// add function
+add.onclick = function add() {
+    // we are going to create the list items here
+    const newItem = document.createElement('li');
+    // append
+    list.append(newItem);
+    // modify text content
+    console.log('button has been clicked');
+}
