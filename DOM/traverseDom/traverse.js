a80a8045b1f1022a136fe11a425da2a6c7557620
Youcef <youcefcopy@gmail.com>
2025-11-10 16:46:45 +0100

    Parent traversal

--- DOM/traverseDom/traverse.js
+++ DOM/traverseDom/traverse.js
@@ -3,6 +3,12 @@
 // Traverse the DOM
 
 // Parent Node traversal
+const ul = document.querySelector('ul');
+console.log(ul.parentNode);
+console.log(ul.parentNode.parentNode);
+
+console.log(ul.parentElement);
+
 
 // Child Node traversal
 
