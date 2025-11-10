b489bb48d7943d1b521ccaf9bce5e60da53a4bcb
Youcef <youcefcopy@gmail.com>
2025-11-10 08:36:51 +0100

    Color functionality is functional

--- ColorFlipper/color.js
+++ ColorFlipper/color.js
@@ -22,20 +22,31 @@ const purpleBtn = document.getElementById('purple');
 
 redBtn.onclick = () => {
     console.log('red has been clicked');
+    document.body.style.backgroundColor = 'red';
 }
 
 orangeBtn.onclick = () => {
     console.log('orange has been clicked');
+    document.body.style.backgroundColor = 'orange';
+
 }
 blueBtn.onclick = () => {
     console.log('blue has been clicked');
+    document.body.style.backgroundColor = 'blue';
+
 }
 yellowBtn.onclick = () => {
     console.log('yellow has been clicked');
+    document.body.style.backgroundColor = 'yellow';
+
 }
 greenBtn.onclick = () => {
     console.log('green has been clicked');
+    document.body.style.backgroundColor = 'green';
+
 }
 purpleBtn.onclick = () => {
     console.log('purple has been clicked');
+    document.body.style.backgroundColor = 'purple';
+
 }
