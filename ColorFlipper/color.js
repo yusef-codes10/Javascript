267142029ae5f9786c393db5a538dc6009e67661
Youcef <youcefcopy@gmail.com>
2025-11-10 08:41:29 +0100

    HEX values are functional

--- ColorFlipper/color.js
+++ ColorFlipper/color.js
@@ -19,34 +19,46 @@ const greenBtn = document.getElementById('green');
 const yellowBtn = document.getElementById('yellow');
 const orangeBtn = document.getElementById('orange');
 const purpleBtn = document.getElementById('purple');
+const colorValue = document.querySelector('span');
 
 redBtn.onclick = () => {
     console.log('red has been clicked');
     document.body.style.backgroundColor = 'red';
+    colorValue.textContent = '#ff0000';
 }
 
 orangeBtn.onclick = () => {
     console.log('orange has been clicked');
     document.body.style.backgroundColor = 'orange';
+    colorValue.textContent = '#ffa500';
+
 
 }
 blueBtn.onclick = () => {
     console.log('blue has been clicked');
     document.body.style.backgroundColor = 'blue';
+    colorValue.textContent = '#0000ff';
+    
 
 }
 yellowBtn.onclick = () => {
     console.log('yellow has been clicked');
     document.body.style.backgroundColor = 'yellow';
+    colorValue.textContent = '#ffff00';
+
 
 }
 greenBtn.onclick = () => {
     console.log('green has been clicked');
     document.body.style.backgroundColor = 'green';
+    colorValue.textContent = '#008000';
+
 
 }
 purpleBtn.onclick = () => {
     console.log('purple has been clicked');
     document.body.style.backgroundColor = 'purple';
+    colorValue.textContent = '#800080';
+
 
 }
