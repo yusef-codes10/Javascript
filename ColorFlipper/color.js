24a587ba745aae6bfa5bdfae5e35580fba6b6b85
Youcef <youcefcopy@gmail.com>
2025-11-10 08:15:04 +0100

    colors div aree acts now as buttons

--- ColorFlipper/color.js
+++ ColorFlipper/color.js
@@ -11,3 +11,31 @@ change.onclick = () => {
     
     console.log('btn has been clicked');
 }
+
+// clickable div
+const redBtn = document.getElementById('red');
+const blueBtn = document.getElementById('blue');
+const greenBtn = document.getElementById('green');
+const yellowBtn = document.getElementById('yellow');
+const orangeBtn = document.getElementById('orange');
+const purpleBtn = document.getElementById('purple');
+
+redBtn.onclick = () => {
+    console.log('red has been clicked');
+}
+
+orangeBtn.onclick = () => {
+    console.log('orange has been clicked');
+}
+blueBtn.onclick = () => {
+    console.log('blue has been clicked');
+}
+yellowBtn.onclick = () => {
+    console.log('yellow has been clicked');
+}
+greenBtn.onclick = () => {
+    console.log('green has been clicked');
+}
+purpleBtn.onclick = () => {
+    console.log('purple has been clicked');
+}
