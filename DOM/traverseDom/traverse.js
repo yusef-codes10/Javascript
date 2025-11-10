4ea1586130f2248e145014e47815576cc4110b93
Youcef <youcefcopy@gmail.com>
2025-11-10 20:36:25 +0100

    child traverse example

--- DOM/traverseDom/traverse.js
+++ DOM/traverseDom/traverse.js
@@ -9,5 +9,10 @@ console.log(ul.parentElement);
 
 // Child Node traversal
 console.log(ul.childNodes);
+console.log(ul.firstChild);
+console.log(ul.lastChild);
+
+ul.childNodes[1].style.backgroundColor = "blue";
+
 
 // Siblings traversal
