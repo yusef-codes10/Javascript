62bb61ecaa77d018464daf06c130011b639546e0
Youcef <youcefcopy@gmail.com>
2025-11-10 16:51:03 +0100

    child traverse

--- DOM/traverseDom/traverse.js
+++ DOM/traverseDom/traverse.js
@@ -5,11 +5,9 @@
 // Parent Node traversal
 const ul = document.querySelector('ul');
 console.log(ul.parentNode);
-console.log(ul.parentNode.parentNode);
-
 console.log(ul.parentElement);
 
-
 // Child Node traversal
+console.log(ul.childNodes);
 
 // Siblings traversal
