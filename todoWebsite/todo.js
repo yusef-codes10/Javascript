1522957a3fccef94e17d075b1125c7d7be2c190d
Youcef <youcefcopy@gmail.com>
2025-11-11 21:18:45 +0100

    JS | check function toggle has been addedd (text decoration)

--- todoWebsite/todo.js
+++ todoWebsite/todo.js
@@ -1,46 +1,43 @@
-const list = document.querySelector('ul');
-const btn = document.getElementById('btn');
-
-
+const list = document.querySelector("ul");
+const btn = document.getElementById("btn");
 
 btn.onclick = () => {
-    // console.log('btn has been clicked');
-    const newItem = document.createElement('div');
-    newItem.style.backgroundColor = 'red';
-    newItem.classList.add('div');
+  // console.log('btn has been clicked');
+  const newItem = document.createElement("div");
+  newItem.style.backgroundColor = "red";
+  newItem.classList.add("div");
 
-    const btnNew = document.createElement('button');
-    btnNew.classList.add('btn');
-    newItem.append(btnNew);
+  const btnNew = document.createElement("button");
+  btnNew.classList.add("btn");
+  newItem.append(btnNew);
 
-    // the text
-    const todoText = document.createElement('p');
-    todoText.textContent = 'You have to do it';
-    newItem.append(todoText);
+  // the text
+  const todoText = document.createElement("p");
+  todoText.textContent = "You have to do it";
+  newItem.append(todoText);
 
-    // remove icon
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('btn');
-    newItem.append(removeBtn);
+  // remove icon
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("btn");
+  newItem.append(removeBtn);
 
-    // check functionality
-    btnNew.onclick = () => {
-        todoText.style.textDecoration = 'line-through';
+  // check functionality
+  btnNew.onclick = () => {
+    if (todoText.style.textDecoration === "line-through") {
+      todoText.style.textDecoration = "none";
+    } else {
+      todoText.style.textDecoration = "line-through";
     }
-    
-    // remove functionality
-    removeBtn.onclick = () => {
-        newItem.remove();
-    }
+  };
 
-    list.append(newItem);
-    console.log('div has been created');
+  // remove functionality
+  removeBtn.onclick = () => {
+    newItem.remove();
+  };
 
-
-
-
-}
-
+  list.append(newItem);
+  console.log("div has been created");
+};
 
 // todo check btn | rename, add toggle functionality
 // todo add icons for the buttons
