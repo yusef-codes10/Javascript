deef7e7365b0fdee93b5067de185bc2a99f317e5
Youcef <youcefcopy@gmail.com>
2025-11-11 08:50:19 +0100

    style js | text & button

--- todoWebsite/todo.js
+++ todoWebsite/todo.js
@@ -6,9 +6,17 @@ const btn = document.getElementById('btn');
 btn.onclick = () => {
     // console.log('btn has been clicked');
     const newItem = document.createElement('div');
+    newItem.style.backgroundColor = 'red';
+    newItem.classList.add('div');
+
     const btnNew = document.createElement('button');
     btnNew.classList.add('btn');
     newItem.append(btnNew);
+
+    // the text
+    const todoText = document.createElement('p');
+    todoText.textContent = 'You have to do it';
+    newItem.append(todoText);
 
     list.append(newItem);
     console.log('div has been created');
