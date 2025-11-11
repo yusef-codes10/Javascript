a7d356039f0a68c25f6abb2a065ae942d9c232b5
Youcef <youcefcopy@gmail.com>
2025-11-11 08:41:46 +0100

    targeting css class is better than tareting button tag

--- todoWebsite/todo.js
+++ todoWebsite/todo.js
@@ -1,2 +1,19 @@
 const list = document.querySelector('ul');
-console.log(list);
+const btn = document.getElementById('btn');
+
+
+
+btn.onclick = () => {
+    // console.log('btn has been clicked');
+    const newItem = document.createElement('div');
+    const btnNew = document.createElement('button');
+    btnNew.classList.add('button');
+    newItem.append(btnNew);
+
+    list.append(newItem);
+    console.log('div has been created');
+
+
+
+
+}
