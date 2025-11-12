51bdcbf9320c9843a9fdc142d262c4df691c83f9
Youcef <youcefcopy@gmail.com>
2025-11-12 12:20:35 +0100

    the code is messy | Another approach

--- todoWebsite/todo.js
+++ todoWebsite/todo.js
@@ -5,53 +5,55 @@ const inputTask = document.createElement("input");
 btn.addEventListener("click", mainFunc);
 btn.addEventListener("click", createToDo);
 function mainFunc() {
-  // console.log('btn has been clicked');
-  const newItem = document.createElement("div");
-  newItem.classList.add("div");
+  if (getUserInput() !== "") {
+    // console.log('btn has been clicked');
+    const newItem = document.createElement("div");
+    newItem.classList.add("div");
 
-  const btnNew = document.createElement("button");
-  btnNew.classList.add("btn");
-  newItem.append(btnNew);
+    const btnNew = document.createElement("button");
+    btnNew.classList.add("btn");
+    newItem.append(btnNew);
 
-  // // create a new input
-  // const inputDiv = document.createElement('div');
-  // inputDiv.classList.add('div');
-  // inputDiv.style.backgroundColor = 'red';
-  // newItem.append(inputDiv);
-  // the text
-  const todoText = document.createElement("p");
-  todoText.textContent = getUserInput();
-  newItem.append(todoText);
+    // // create a new input
+    // const inputDiv = document.createElement('div');
+    // inputDiv.classList.add('div');
+    // inputDiv.style.backgroundColor = 'red';
+    // newItem.append(inputDiv);
+    // the text
+    const todoText = document.createElement("p");
+    todoText.textContent = getUserInput();
+    newItem.append(todoText);
 
-  // remove icon
-  const removeBtn = document.createElement("button");
-  removeBtn.classList.add("btn");
-  newItem.append(removeBtn);
+    // remove icon
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("btn");
+    newItem.append(removeBtn);
 
-  // check icon
-  const checkIcon = document.createElement("li");
-  checkIcon.className = "bi bi-check";
-  btnNew.appendChild(checkIcon);
-  // check functionality
-  btnNew.onclick = () => {
-    if (todoText.style.textDecoration === "line-through") {
-      todoText.style.textDecoration = "none";
-    } else {
-      todoText.style.textDecoration = "line-through";
-    }
-  };
+    // check icon
+    const checkIcon = document.createElement("li");
+    checkIcon.className = "bi bi-check";
+    btnNew.appendChild(checkIcon);
+    // check functionality
+    btnNew.onclick = () => {
+      if (todoText.style.textDecoration === "line-through") {
+        todoText.style.textDecoration = "none";
+      } else {
+        todoText.style.textDecoration = "line-through";
+      }
+    };
 
-  // create the icon
-  const trashIcon = document.createElement("i");
-  trashIcon.className = "bi bi-trash-fill";
-  removeBtn.appendChild(trashIcon);
-  // remove functionality
-  removeBtn.onclick = () => {
-    newItem.remove();
-  };
+    // create the icon
+    const trashIcon = document.createElement("i");
+    trashIcon.className = "bi bi-trash-fill";
+    removeBtn.appendChild(trashIcon);
+    // remove functionality
+    removeBtn.onclick = () => {
+      newItem.remove();
+    };
 
-  list.append(newItem);
-  console.log("div has been created");
+    list.append(newItem);
+    console.log("div has been created");
+  }
 }
 
 // todo check btn | rename, add toggle functionality
