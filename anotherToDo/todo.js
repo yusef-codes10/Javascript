04c341a8bbf593fbeaec3b7b829f6908a6d2a2c2
Youcef <youcefcopy@gmail.com>
2025-11-13 16:05:44 +0100

    complete it later

--- anotherToDo/todo.js
+++ anotherToDo/todo.js
@@ -1,7 +1,21 @@
 const userInput = document.getElementById("userInput");
 const addTask = document.getElementById("addTask");
-addTask.addEventListener("click", () => console.log(userInput));
+addTask.addEventListener("click", addTaskFunction);
 
-function addTask() {
-    
+function addTaskFunction() {
+  // create a div
+  const taskItem = document.createElement("div");
+  const taskContainer = document.querySelector(".task-container");
+
+  // create a p tag inside
+  const taskPar = document.createElement("p");
+  taskPar.classList.add("new-item");
+
+  taskPar.innerHTML = "hello there";
+  taskContainer.append(taskPar);
+
+  taskContainer.append(taskItem);
 }
+
+
+// todo paused for later
