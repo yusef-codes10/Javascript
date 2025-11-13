9c7c9d166e56cac195c84ff5c1de4ae392527122
Youcef <youcefcopy@gmail.com>
2025-11-13 19:27:25 +0100

    minor chnages

--- DOM/evenPropogation/script.js
+++ DOM/evenPropogation/script.js
@@ -1,2 +1,6 @@
 // Event Probagation
 // Event propagation in JavaScript describes the process by which events travel through the Document Object Model (DOM) tree when triggered on an element, allowing event handlers on parent or ancestor elements to respond
+// Three Phases:
+// !1- Event Capturing
+// !2- Target
+// !3- Event Bubbling
