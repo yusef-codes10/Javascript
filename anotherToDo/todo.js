c9a2c0e758c13ea3958cf506a90ec02c3f941a3d
Youcef <youcefcopy@gmail.com>
2025-11-13 15:30:10 +0100

    JS has been initilaized

---
+++ anotherToDo/todo.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById("userInput");
+const addTask = document.getElementById("addTask");
+addTask.addEventListener("click", () => console.log(userInput));
+
+function addTask() {
+    
+}
