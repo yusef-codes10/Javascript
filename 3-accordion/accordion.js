850b292e60e3dbc5d4197bdb9005a407606e45c2
Youcef <youcefcopy@gmail.com>
2025-11-15 20:23:50 +0100

    loop & event listener

--- 3-accordion/accordion.js
+++ 3-accordion/accordion.js
@@ -4,3 +4,10 @@
 // for loop
 // this
 // classlist.toggle()
+const accordion = document.getElementsByClassName('content-container');
+
+// ! we wanna access all classes with a for loop 
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', () => console.log('clicked'));
+    
+}
