86dc0f85ab00a3f376649907aaff06b6c87fedcf
Youcef <youcefcopy@gmail.com>
2025-11-15 10:53:05 +0100

    toggle functionality

--- modalUI/script.js
+++ modalUI/script.js
@@ -5,3 +5,8 @@
 // e.target
 // CSS styling through JavaScript
 // CSS Animation
+
+const openBtn = document.getElementById('openBtn');
+const popUp = document.querySelector('.pop-up');
+openBtn.addEventListener('click', () => popUp.classList.toggle('hidden')) ;
+
