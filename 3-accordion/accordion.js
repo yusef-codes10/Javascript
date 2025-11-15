d7ae3e3e3fb4b61c5f73177d76b061f7afcbc8e3
Youcef <youcefcopy@gmail.com>
2025-11-15 22:16:46 +0100

    class toggle needs improvements

--- 3-accordion/accordion.js
+++ 3-accordion/accordion.js
@@ -8,6 +8,10 @@ const accordion = document.getElementsByClassName('content-container');
 
 // ! we wanna access all classes with a for loop 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', () => console.log('clicked'));
+    accordion[i].addEventListener('click', function() {
+        console.log('clicked');
+        // using the 'this' 
+        this.classList.toggle('active');
+    });
     
 }
