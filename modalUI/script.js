28c56cd0b0d3b44722f8a0f20793cb41004ad9f3
Youcef <youcefcopy@gmail.com>
2025-11-15 13:50:55 +0100

    clicking outside must have an overlay

--- modalUI/script.js
+++ modalUI/script.js
@@ -6,9 +6,18 @@
 // CSS styling through JavaScript
 // CSS Animation
 
-const openBtn = document.getElementById('openBtn');
-const popUp = document.querySelector('.pop-up');
-const closeBtn = document.getElementById('closeBtn');
+const openBtn = document.getElementById("openBtn");
+const popUp = document.querySelector(".pop-up");
+const closeBtn = document.getElementById("closeBtn");
+const overlay = document.querySelector(".overlay");
 
-openBtn.addEventListener('click', () => popUp.classList.toggle('hidden')) ;
+openBtn.addEventListener("click", () => popUp.classList.toggle("hidden"));
+closeBtn.addEventListener("click", () => popUp.classList.add("hidden"));
 
+// ! click anywhere outside of the model to exist
+window.addEventListener("click", (e) => {
+  if (popUp.contains(e.target)) {
+    popUp.classList.add("hidden");
+    console.log("clicked");
+  }
+});
