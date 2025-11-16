a021a23b9564beab4917c56e0ddb4e962f1b7cdf
Youcef <youcefcopy@gmail.com>
2025-11-16 13:05:52 +0100

    draft js code

--- 4-stopWatch/stop.js
+++ 4-stopWatch/stop.js
@@ -6,3 +6,19 @@
 // toString()
 // setInterval()
 // innerHTML
+
+const playBtn = document.getElementById('play');
+const timerContent = document.getElementById('timer');
+
+playBtn.addEventListener('click', () => console.log('clicked'));
+
+function play() {
+    let counter = 0;
+    while (true) {
+        timerContent.textContent = counter;
+        if (counter === 10) {
+            return;
+        }
+        counter++;
+    }
+}
