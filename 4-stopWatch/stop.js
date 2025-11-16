cd801bd6fbc501b5e80410d7cf677c0809879041
Youcef <youcefcopy@gmail.com>
2025-11-16 13:11:18 +0100

    stopWatch logic has been added

--- 4-stopWatch/stop.js
+++ 4-stopWatch/stop.js
@@ -15,3 +15,19 @@ const resetBtn = document.querySelector('#resetBtn');
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+
+// stop watch function 
+function stopWatch() {
+
+    seconds++;
+    if (seconds / 60 === 1) {
+        seconds = 0;
+        minutes++;
+
+        if (minutes / 60 === 1) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+}
