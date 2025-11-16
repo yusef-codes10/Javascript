f3be07cd814a1fe749a4422f0af55b5e3a7cd0b1
Youcef <youcefcopy@gmail.com>
2025-11-16 14:42:36 +0100

    leading zeros variables have been added

--- 4-stopWatch/stop.js
+++ 4-stopWatch/stop.js
@@ -8,38 +8,57 @@
 // innerHTML
 
 // buttons
-const stopBtn = document.querySelector('#startStopBtn');
-const resetBtn = document.querySelector('#reset');
+const stopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#reset");
 
 // time values
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-// stop watch function 
+// leading zeros
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// stop watch function
 function stopWatch() {
+  seconds++;
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
 
-    seconds++;
-    if (seconds / 60 === 1) {
-        seconds = 0;
-        minutes++;
-
-        if (minutes / 60 === 1) {
-            minutes = 0;
-            hours++;
-        }
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
     }
+  }
+  // include leading zeros
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
 
-    let displayTimer = document.getElementById('timer').innerText 
-    = hours + ':' + minutes + ':' + seconds;
-    
+  let displayTimer = (document.getElementById("timer").innerText =
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
 const playInterval = window.setInterval(stopWatch, 1000);
 
 function setTimeOut() {
-    clearInterval(playInterval);
-    console.log('stopped');
+  clearInterval(playInterval);
+  console.log("stopped");
 }
 
-resetBtn.addEventListener('click', setTimeOut);
+resetBtn.addEventListener("click", setTimeOut);
