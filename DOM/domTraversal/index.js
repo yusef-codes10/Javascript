68682447c237e09d4a36a8df0a6f99334f0877a7
Youcef <youcefcopy@gmail.com>
2025-11-18 17:06:02 +0100

    next plan

--- DOM/domTraversal/index.js
+++ DOM/domTraversal/index.js
@@ -8,3 +8,10 @@ childen.forEach(changeColor)
 function changeColor(element) {
     element.style.backgroundColor = '#333111';
 }
+
+
+
+
+
+
+// ! Why sometimes we have to convert into an array before using forEach (className) and sometimes we do not (querySelectorAll)
