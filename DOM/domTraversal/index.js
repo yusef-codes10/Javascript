01983d7c54a263a895da373d1bf9c0cc273d7507
Youcef <youcefcopy@gmail.com>
2025-11-18 15:35:05 +0100

    selecting the children | have to convert into an array

--- DOM/domTraversal/index.js
+++ DOM/domTraversal/index.js
@@ -1,16 +1,10 @@
 // logic
 // by id
-const grandParent = document.getElementById('grandParent');
-
+const grandParent = document.querySelector('.grand-parent');
+// selecting the childen
+const childen = Array.from(grandParent.children);
+childen.forEach(changeColor)
 
 function changeColor(element) {
     element.style.backgroundColor = '#333111';
 }
-
-changeColor(grandParent);
-
-
-// by className
-const parents = Array.from(document.getElementsByClassName('parent'));
-// using foreach
-parents.forEach(changeColor);
