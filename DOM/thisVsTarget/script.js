043d76f8bd9e51e49a4ae712af4115bfe26e1719
Youcef <youcefcopy@gmail.com>
2025-11-20 21:46:02 +0100

    using e.target to circle throuh multiple elements with one signle event listener

--- DOM/thisVsTarget/script.js
+++ DOM/thisVsTarget/script.js
@@ -1,8 +1,17 @@
-const box = document.querySelector('#box');
-// const btn = document.querySelector('#btn');
+// const box = document.querySelector('#box');
+// // const btn = document.querySelector('#btn');
 
-box.addEventListener('click', function (e) {
-    console.log(this);  // the box itsef
-    console.log(e); // the event
-    console.log(e.target);  // the button itself
-})
+// box.addEventListener('click', function (e) {
+//     console.log(this);  // the box itsef
+//     console.log(e); // the event
+//     console.log(e.target);  // the button itself
+// })
+
+const list = document.querySelector("#list");
+list.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("done");
+  }
+  console.log(e.target);
+  console.log(this);
+});
