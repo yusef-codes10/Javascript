35a146dc8b8a12ae5a406c045ba56d177fae5fb6
Youcef <youcefcopy@gmail.com>
2025-11-20 21:32:05 +0100

    this is whatever the event is attached to & e.target is whatever is clicked

--- DOM/thisVsTarget/script.js
+++ DOM/thisVsTarget/script.js
@@ -1,5 +1,8 @@
 const box = document.querySelector('#box');
+// const btn = document.querySelector('#btn');
 
-box.addEventListener('click', function () {
-    console.log(this);  // the box container
+box.addEventListener('click', function (e) {
+    console.log(this);  // the box itsef
+    console.log(e); // the event
+    console.log(e.target);  // the button itself
 })
