7d98739f46aeece3cb065ac285e72b12351418c0
Youcef <youcefcopy@gmail.com>
2025-11-21 16:38:48 +0100

    preparing the dialog example

--- DOM/thisVsTarget/eventDelegation.js
+++ DOM/thisVsTarget/eventDelegation.js
@@ -1,10 +1,14 @@
 // !2- using event delegation
 // attach the event listener to a parent
-const container = document.querySelector('.container');
-container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('fa-xmark')) {
-        e.target.parentElement.classList.toggle('hidden');
-        console.log('icons has been clicked');
-    }
-    console.log('event fired');
-})
+const container = document.querySelector(".container");
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-xmark")) {
+    e.target.parentElement.classList.toggle("hidden");
+    console.log("icons has been clicked");
+
+  }
+  console.log("event fired");
+  // TODO check these logs out
+  console.log(e.target);
+  console.log(this);
+});
