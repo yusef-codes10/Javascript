9c29e85d15d248aecdc0127ea120ed81001f1fd2
Youcef <youcefcopy@gmail.com>
2025-11-21 18:57:03 +0100

    bugs fixed

--- DOM/thisVsTarget/dialog.js
+++ DOM/thisVsTarget/dialog.js
@@ -12,3 +12,14 @@ function openDialog() {
 function closeDialog() {
     dialog.close();
 }
+
+// ! using event delegation
+const cardsContainer = document.querySelector('.cards');
+console.log(cardsContainer);
+cardsContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('close-card')) {
+        console.log('closing the card');
+        e.target.parentElement.classList.toggle('hidden');
+    }
+    console.log('Oops! you are touching the card!');
+})
